a9a719b6e7bcdadb0b3cc72b632890c67fcca7c6
ITStore <ITStore@MENNA>
2026-01-08 08:01:08 +0200

    Create Verify Code & Set New Password pages (UI only)

--- khdamatk.client/src/App.jsx
+++ khdamatk.client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Signup from './Pages/Signup/Signup';
 import Login from './Pages/Login/Login';
+import ForgetPassword from './Pages/ForgetPassword/ForgetPassword';
+import VerifyCode from './Pages/VerifyCode/VerifyCode';
+import SetNewPassword from './Pages/SetNewPassword/SetNewPassword';
 
 function App() {
     return (
         <>
             {/* <Signup/> */}
-            <Login/>
+            {/* <Login/> */}
+            {/* <ForgetPassword/> */}
+            {/* <VerifyCode/> */}
+            <SetNewPassword/>
         </>
     );
 }
