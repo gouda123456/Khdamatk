ddd6be0d09c9edbd2c417dcc602b3184ed33e135
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2026-01-21 13:38:17 +0200

    Fix2

--- Khdamatk.Server/Data/Configurations/IdentityConfig/UserConfigurations.cs
+++ Khdamatk.Server/Data/Configurations/IdentityConfig/UserConfigurations.cs
@@ -6,6 +6,9 @@ public class UserConfigurations : IEntityTypeConfiguration<User>
     {
         builder.HasData(DefaultUsersData.Admin, DefaultUsersData.Member);
 
-        
+        builder.HasOne(sp => sp.ProfilePicture)
+           .WithOne()
+           .HasForeignKey<User>(sp => sp.ProfilePicture)
+           .OnDelete(DeleteBehavior.SetNull);
     }
 }
