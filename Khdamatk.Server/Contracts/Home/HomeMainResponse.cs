7577fa02667a96db4ba32af827b61655cd6bfb1a
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2026-02-03 22:30:02 +0200

    Fix Conflicts

--- Khdamatk.Server/Contracts/Home/HomeMainResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Khdamatk.Server.Contracts.Home
-{
-    public record HomeMainResponse(List<string> Categories,List<FreelancerHomePageCard> Freelancers);
-    public record FreelancerHomePageCard(string ProfileImagePath , string Name, string job );
-}
