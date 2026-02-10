c23a2bb792e4f05213ef715c1af7b793911ba032
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2026-02-10 14:26:01 +0200

    Add Clolumns for security the Media

--- Khdamatk.Server/Data/Entities/Catalog/Media.cs
+++ Khdamatk.Server/Data/Entities/Catalog/Media.cs
@@ -6,7 +6,10 @@ public record Media
     public int Id { get; init; }
 
     public string? FileName { get; init; }
+    public string StoredFileName { get; set; } = string.Empty;
+
     public string? ContentType { get; init; }
+    public string FileExtension { get; set; } = string.Empty;
     public long Size { get; init; }
 
     public string FullPath => $"{ConstFilePathes.ImagesPath}{Id}";
--- Khdamatk.Server/GlobalUsing.cs
+++ Khdamatk.Server/GlobalUsing.cs
@@ -1,5 +1,4 @@
-﻿
-global using System.ComponentModel;
+﻿global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using Microsoft.AspNetCore.Identity;
