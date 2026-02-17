2549e7a8287ba70c117945d431005006965d8742
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2026-02-17 17:56:14 +0200

    seperate portifilo media and item to 2 files

--- Khdamatk.Server/Data/Entities/Catalog/PortfolioItem.cs
+++ Khdamatk.Server/Data/Entities/Catalog/PortfolioItem.cs
@@ -29,14 +29,3 @@ public class PortfolioItem : BaseEntity
 
     public virtual ServiceProviderProfile ServiceProviderProfile { get; set; } = null!;
 }
-
-public class PortfolioMedia
-{
-    // 1. المفاتيح الأجنبية التي تشكل المفتاح المركب
-    public int PortfolioItemId { get; set; }
-    public int MediaId { get; set; }
-
-    // 2. خصائص التنقل
-    public virtual PortfolioItem PortfolioItem { get; set; } = null!;
-    public virtual Media Media { get; set; } = null!;
-}
---
+++ Khdamatk.Server/Data/Entities/Catalog/PortfolioMedia.cs
@@ -0,0 +1,12 @@
+﻿namespace Khdamatk.Server.Data.Entities.Catalog;
+
+public class PortfolioMedia
+{
+    // 1. المفاتيح الأجنبية التي تشكل المفتاح المركب
+    public int PortfolioItemId { get; set; }
+    public int MediaId { get; set; }
+
+    // 2. خصائص التنقل
+    public virtual PortfolioItem PortfolioItem { get; set; } = null!;
+    public virtual Media Media { get; set; } = null!;
+}
