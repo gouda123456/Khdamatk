22dfd002a124494f6ed3c6d0cdde4b0e37f11a50
youssef <youssouffathy@gmail.com>
2026-02-25 00:10:14 +0200

    move home funcions to services provider

--- Khdamatk.Server/Services/Interfaces/IHomeService.cs
+++ Khdamatk.Server/Services/Interfaces/IHomeService.cs
@@ -7,12 +7,5 @@ public interface IHomeService : IService
 {
     Task<resultBase> MainPage(CancellationToken cancellationToken);
     Task<resultBase> JobsPage(string? service, CancellationToken cancellationToken);
-    Task<resultBase> FreelancersPage(FreelancerRequest freelancerRequest, CancellationToken cancellationToken);
-    Task<resultBase> FreelancerProfile(string userId, CancellationToken cancellationToken);
-    Task<resultBase> UpdateProfileBasicInfo(string userId, UpdateProfileRequest request);
-    Task<resultBase> AddPortfolioItem(string userId, AddPortfolioRequest request);
-    Task<resultBase> AddEducation(string userId, AddEducationRequest request);
-    Task<resultBase> AddExperience(string userId, AddExperienceRequest request);
-    Task<resultBase> DeletePortfolioItem(string userId, int itemId);
-    Task<resultBase> UpdateSkills(string userId, UpdateSkillsRequest request);
+   
 }
--- Khdamatk.Server/Services/Interfaces/IServiceProviderService.cs
+++ Khdamatk.Server/Services/Interfaces/IServiceProviderService.cs
@@ -1,5 +1,15 @@
-﻿namespace Khdamatk.Server.Services.Interfaces;
+﻿using Khdamatk.Server.Contracts.Home;
+
+namespace Khdamatk.Server.Services.Interfaces;
 
 public interface IServiceProviderService : IService
 {
+    Task<resultBase> FreelancersPage(FreelancerRequest freelancerRequest, CancellationToken cancellationToken);
+    Task<resultBase> FreelancerProfile(string userId, CancellationToken cancellationToken);
+    Task<resultBase> UpdateProfileBasicInfo(string userId, UpdateProfileRequest request);
+    Task<resultBase> AddPortfolioItem(string userId, AddPortfolioRequest request);
+    Task<resultBase> AddEducation(string userId, AddEducationRequest request);
+    Task<resultBase> AddExperience(string userId, AddExperienceRequest request);
+    Task<resultBase> DeletePortfolioItem(string userId, int itemId);
+    Task<resultBase> UpdateSkills(string userId, UpdateSkillsRequest request);
 }
