4c235342ad339e1bd393690c3a66f7abe4d55f95
youssef <youssouffathy@gmail.com>
2026-02-26 15:51:06 +0200

    change name form requstes and serviceProvider

--- Khdamatk.Server/Controllers/V1/HomeController.cs
+++ Khdamatk.Server/Controllers/V1/HomeController.cs
@@ -10,6 +10,7 @@ public class HomeController(IHomeService homeService) : ControllerBase
 {
     private readonly IHomeService homeService = homeService;
 
+
     [HttpGet("")]
     public async Task<IActionResult> GetHomeData(CancellationToken cancellationToken)
     {
@@ -17,59 +18,5 @@ public class HomeController(IHomeService homeService) : ControllerBase
         return result.Respond();
     }
 
-    [HttpGet("Freelancers")]
-    public async Task<IActionResult> GetFreelancers([FromQuery] FreelancerRequest freelancerRequest,CancellationToken cancellationToken)
-    {
-        var result = await homeService.FreelancersPage(freelancerRequest,cancellationToken);
-        return result.Respond();
-    }
 
-
-    [HttpGet("freelancer-profile/{userId}")]
-    public async Task<IActionResult> GetProfile(string userId, CancellationToken cancellationToken)
-    {
-        var result = await homeService.FreelancerProfile(userId, cancellationToken);
-
-        return result.Respond();
-    }
-    [HttpPut("update-basic-info")]
-    public async Task<IActionResult> UpdateInfo(UpdateProfileRequest request)
-    => Ok(await homeService.UpdateProfileBasicInfo(User.GetUserId(), request));
-
-    [HttpPost("portfolio")]
-    public async Task<IActionResult> AddWork(AddPortfolioRequest request)
-        => Ok(await homeService.AddPortfolioItem(User.GetUserId(), request));
-    [HttpPost("add-education")]
-    public async Task<IActionResult> AddEducation([FromBody] AddEducationRequest request)
-    {
-       
-        var userId = User.GetUserId();
-
-        var result = await homeService.AddEducation(userId, request);
-
-        return Ok(result);
-    }
-    [HttpPost("add-experience")]
-    public async Task<IActionResult> AddExperience([FromBody] AddExperienceRequest request)
-    {
-        var userId = User.GetUserId(); // دي الـ Extension method اللي بتجيب الـ ID من الـ Token
-        var result = await homeService.AddExperience(userId, request);
-        return Ok(result);
-    }
-    [HttpDelete("portfolio/{itemId}")]
-    public async Task<IActionResult> DeletePortfolio(int itemId)
-    {
-        var userId = User.GetUserId(); // بنجيب الـ ID من الـ Token لضمان الأمان
-        var result = await homeService.DeletePortfolioItem(userId, itemId);
-
-        return result.Respond();
-    }
-    [HttpPut("update-skills")]
-    public async Task<IActionResult> UpdateSkills([FromBody] UpdateSkillsRequest request)
-    {
-        var userId = User.GetUserId();
-        var result = await homeService.UpdateSkills(userId, request);
-
-        return result.Respond();
-    }
 }
--- Khdamatk.Server/Controllers/V1/ServiceProviderController.cs
+++ Khdamatk.Server/Controllers/V1/ServiceProviderController.cs
@@ -1,10 +1,72 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Khdamatk.Server.Contracts.Home;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Khdamatk.Server.Controllers.V1;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ServiceProviderController : ControllerBase
+public class ServiceProviderController(IServiceProviderService ServiceProviderService) : ControllerBase
 {
+    private readonly IServiceProviderService ServiceProviderService = ServiceProviderService;
+
+    [HttpGet("Freelancers")]
+    public async Task<IActionResult> GetFreelancers([FromQuery] FreelancerRequest freelancerRequest, CancellationToken cancellationToken)
+    {
+        var result = await ServiceProviderService.FreelancersPage(freelancerRequest, cancellationToken);
+        return result.Respond();
+    }
+
+    [HttpGet("freelancer-profile/{userId}")]
+    public async Task<IActionResult> GetProfile(string userId, CancellationToken cancellationToken)
+    {
+        var result = await ServiceProviderService.FreelancerProfile(userId, cancellationToken);
+        return result.Respond();
+    }
+
+    [HttpPut("update-basic-info")]
+    public async Task<IActionResult> UpdateInfo(UpdateProfileRequest request)
+    {
+        var result = await ServiceProviderService.UpdateProfileBasicInfo(User.GetUserId(), request);
+        return result.Respond();
+    }
+
+    [HttpPost("portfolio")]
+    public async Task<IActionResult> AddWork(AddPortfolioRequest request)
+    {
+        var result = await ServiceProviderService.AddPortfolioItem(User.GetUserId(), request);
+        return result.Respond();
+    }
+
+    [HttpPost("add-education")]
+    public async Task<IActionResult> AddEducation([FromBody] AddEducationRequest request)
+    {
+        var userId = User.GetUserId();
+        var result = await ServiceProviderService.AddEducation(userId, request);
+        return result.Respond();
+    }
+
+    [HttpPost("add-experience")]
+    public async Task<IActionResult> AddExperience([FromBody] AddExperienceRequest request)
+    {
+        var userId = User.GetUserId();
+        var result = await ServiceProviderService.AddExperience(userId, request);
+        return result.Respond();
+    }
+
+    [HttpDelete("portfolio/{itemId}")]
+    public async Task<IActionResult> DeletePortfolio(int itemId)
+    {
+        var userId = User.GetUserId();
+        var result = await ServiceProviderService.DeletePortfolioItem(userId, itemId);
+        return result.Respond();
+    }
+
+    [HttpPut("update-skills")]
+    public async Task<IActionResult> UpdateSkills([FromBody] UpdateSkillsRequest request)
+    {
+        var userId = User.GetUserId();
+        var result = await ServiceProviderService.UpdateSkills(userId, request);
+        return result.Respond();
+    }
 }
