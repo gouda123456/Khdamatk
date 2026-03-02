ea8d883c6c39e73c235770ab95967937ccc2e2cc
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2026-03-02 23:38:14 +0200

    Revert "trst123"
    This reverts commit ab367419d337591bf96a29c97c708aeaa5a9d492.

--- Khdamatk.Server/DependancyInjections.cs
+++ Khdamatk.Server/DependancyInjections.cs
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 ﻿using Asp.Versioning;
 using Khdamatk.Server.Services;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
-=======
-﻿using System.Text.Json.Serialization;
-using Asp.Versioning;
-using Khdamatk.Server.Helper.Payment;
-using Microsoft.OpenApi.Models;
-using Stripe;
-using Stripe.BillingPortal;
->>>>>>> 0d1a058297a2960869e173befa070e5eabf88e53
 
 namespace Khdamatk.Server;
 
@@ -20,7 +11,7 @@ public static class DependancyInjections
     {
         services.AddDbContext<Database>(options =>
             options.UseLazyLoadingProxies().UseSqlServer(
-                configuration.GetConnectionString("YoussefFathy"),
+                configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(Database).Assembly.FullName)));
 
         services.AddHttpContextAccessor();
@@ -54,55 +45,10 @@ public static class DependancyInjections
         services.AddScoped<IServiceProviderService, ServiceProviderService>();
         services.AddScoped<IJobService, JobService>();
         services.AddScoped<IServiceProviderService, ServiceProviderService>();
-<<<<<<< HEAD
         services.AddScoped<IAdminDashboardSerivce, AdminDashboardService>();
 
-        services.AddScoped<IUserDashboardService, UserDashboardSerivce>();
+        services.AddScoped<IUserDashboardService, UserDashoardService>();
         services.AddScoped<IReportDashboardService, ReportDashboardService>();
-=======
-        services.AddScoped<IOrderService, OrderService>();
-        return services;
-    }
-
-    public static IServiceCollection AddPaymentMethod(this IServiceCollection services, IConfiguration configuration)
-    {
-        //Stripe
-        services.AddOptions<StripeSetting>()
-            .BindConfiguration(nameof(StripeSetting))
-            .ValidateDataAnnotations()
-            .ValidateOnStart();
-
-        StripeConfiguration.AppInfo = new AppInfo
-        {
-            Name = "Khdamatk API",
-            Version = "1.0.0",
-            Url = "https://khdamatk.com",
-            PartnerId = "pp_partner_123456789"
-        };
-        StripeConfiguration.ApiKey = configuration.GetSection(nameof(StripeSetting)).Get<StripeSetting>()!.SecretKey;
-        //services.AddScoped<TokenService>();
-        services.AddScoped<CustomerService>();
-        services.AddScoped<PaymentIntentService>();
-        services.AddScoped<RefundService>();
-        services.AddScoped<ProductService>();
-        services.AddScoped<SubscriptionService>();
-        services.AddScoped<PriceService>();
-        services.AddScoped<InvoiceService>();
-        services.AddScoped<ChargeService>();
-        services.AddScoped<RefundService>();
-        services.AddScoped<SessionService>();
-
-        //Fawaterak
-        services.AddOptions<FawaterakSettings>()
-            .BindConfiguration(nameof(FawaterakSettings))
-            .ValidateDataAnnotations()
-            .ValidateOnStart();
-        services.AddTransient<IFawaterakPaymentHelper, FawaterakPaymentHelper>();
-
-
-
-        services.AddScoped<IPaymentHelper, PaymentHelper>();
->>>>>>> 0d1a058297a2960869e173befa070e5eabf88e53
         return services;
     }
 
