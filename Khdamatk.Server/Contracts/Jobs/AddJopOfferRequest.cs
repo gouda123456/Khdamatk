9ee56073711a90ae3037e0c2ba6a1cfa3d504078
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2026-03-03 21:53:14 +0200

    AddJopOfferRequest

--- Khdamatk.Server/Contracts/Jobs/AddJopOfferRequest.cs
+++ Khdamatk.Server/Contracts/Jobs/AddJopOfferRequest.cs
@@ -1,6 +1,12 @@
 ﻿namespace Khdamatk.Server.Contracts.Jobs;
 
 public record AddJopOfferRequest(
+    string ProviderServiceId, 
     decimal OfferAmount,
-    string CoverLetter
+    string Description,
+    TimeCommitment? TimeCommitment, //TODO: Add TimeCommitment to job Entity
+    ExperienceLevel? ExperienceLevel,
+    DateTime Deadline,
+    byte[]? Attachment,
+    string? SimilarWorkExamples 
     );
---
+++ Khdamatk.Server/Contracts/Jobs/AddJopRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace Khdamatk.Server.Contracts.Jobs;
+
+public record AddJopRequest(
+    string UserId,
+    string Title,
+    string CategoryName,
+    string Description,
+    List<string> Skills,
+    decimal BudgetMin,    
+    decimal BudgetMax,
+    TimeCommitment TimeCommitment, //TODO: Add TimeCommitment to job Entity
+    ExperienceLevel ExperienceLevel,
+    DateTime Deadline
+    );
+public enum TimeCommitment
+{
+    PartTime,
+    FullTime,
+    Hourly,
+    Flexible
+}
