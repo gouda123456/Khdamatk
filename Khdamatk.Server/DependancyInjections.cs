575286f74b5c844825313c2ec046d74177435594
Menna Assem <mennaassem840@gmail.com>
2026-03-04 16:11:09 +0200

    cancel token

--- Khdamatk.Server/DependancyInjections.cs
+++ Khdamatk.Server/DependancyInjections.cs
@@ -10,7 +10,7 @@ public static class DependancyInjections
     {
         services.AddDbContext<Database>(options =>
             options.UseLazyLoadingProxies().UseSqlServer(
-                configuration.GetConnectionString("YoussefFathy"),
+                configuration.GetConnectionString("menna"),
                 b => b.MigrationsAssembly(typeof(Database).Assembly.FullName)));
 
         services.AddHttpContextAccessor();
