e6a68b31efec6d9bbd7de71c1cacbc8f763cfa8b
youssef <youssouffathy@gmail.com>
2026-03-19 01:01:16 +0200

    fix } in order service

--- Khdamatk.Server/Data/Entities/Identity/User.cs
+++ Khdamatk.Server/Data/Entities/Identity/User.cs
@@ -70,6 +70,5 @@ public class User : IdentityUser<string>
 
     public virtual ICollection<JobPost> JobPosts { get; set; } = new List<JobPost>();
 
-    [NotMapped]
-    public virtual bool IsVerified => VerificationData?.Status == VerificationStatus.Approved;
+    
 }
